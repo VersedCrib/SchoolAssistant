327e98c564c0f4a109e2cfa04455e6e7185e40b8
Jedi_of_pascal <artem.portno.39@mail.ru>
2020-01-08 20:53:13 +0300

    make timetable

--- app/src/main/java/com/example/schoolassistant/MainActivity.java
+++ app/src/main/java/com/example/schoolassistant/MainActivity.java
@@ -1,53 +1,55 @@
 package com.example.schoolassistant;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    Timetable timetable[] = new Timetable[5];
-    Journal journal = new Journal(5);
-    int day = 1;
+    int numDay = 1;
+    Day week[] = new Day[5];
+    String weekS[] = new String[5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-/*
-        for(Timetable t: timetable){
-            t = new Timetable();
+
+        
+
+        for(int i =0; i<week.length; i++){
+            week[i] = new Day();
+            week[i].nameDay = dayInWeek(i+1);
         }
 
-
-        timetable[0].homework = (TextView) findViewById(R.id.tw_homework1);
-        timetable[0].lesson = (TextView) findViewById(R.id.tw_lesson1);
-
-        timetable[1].homework = (TextView) findViewById(R.id.tw_homework2);
-        timetable[1].lesson = (TextView) findViewById(R.id.tw_lesson2);
-
-        timetable[2].homework = (TextView) findViewById(R.id.tw_homework3);
-        timetable[2].lesson = (TextView) findViewById(R.id.tw_lesson2);
-
-        timetable[3].homework = (TextView) findViewById(R.id.tw_homework4);
-        timetable[3].lesson = (TextView) findViewById(R.id.tw_lesson4);
-
-        timetable[4].homework = (TextView) findViewById(R.id.tw_homework5);
-        timetable[4].lesson = (TextView) findViewById(R.id.tw_lesson5);
-
-        timetable[5].homework = (TextView) findViewById(R.id.tw_homework6);
-        timetable[5].lesson = (TextView) findViewById(R.id.tw_lesson6);
-
-        timetable[6].homework = (TextView) findViewById(R.id.tw_homework7);
-        timetable[6].lesson = (TextView) findViewById(R.id.tw_lesson7);
-
-        for(int i = 0; i<timetable.length;i++){
-            timetable[i].homework.setText(journal.day[day].getHomework(i));
-            timetable[i].lesson.setText(journal.day[day].getLesson(i));
+        for(int i =0; i<weekS.length; i++){
+            weekS[i] = week[i].nameDay;
         }
 
-*/
+        ListView dayList = (ListView) findViewById(R.id.lw_week);
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,weekS );
+        dayList.setAdapter(adapter);
+
+        dayList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View itemClicked, int position, long id) {
+                TextView textView = (TextView) itemClicked;
+                String strText = textView.getText().toString();
+
+                Intent i = new Intent(MainActivity.this, DayActivity.class);
+                i.putExtra(Day.class.getSimpleName(), week[dayInWeek(strText)]);
+                startActivity(i);
+
+            }
+        });
+
+
 
     }
 
@@ -64,37 +66,37 @@ public class MainActivity extends AppCompatActivity {
         int w = (dayOfMonth + (13 * m - 1 )/5 + year % 100 + (year % 100 )/4  + (year/100)/4 - 2*(year/100)) % 7 ;
         switch (w){
             case 1:
-                day = 1;
+                numDay = 1;
                 nameDay = "Monday";
                 break;
 
             case 2:
-                day = 2;
+                numDay = 2;
                 nameDay = "Tuesday";
                 break;
 
             case 3:
-                day = 3;
+                numDay = 3;
                 nameDay = "Wednesday";
                 break;
 
             case 4:
-                day = 4;
+                numDay = 4;
                 nameDay = "Thursday";
                 break;
 
             case 5:
-                day = 5;
+                numDay = 5;
                 nameDay = "Friday";
                 break;
 
             case 6:
-                day = 6;
+                numDay = 6;
                 nameDay = "Saturday";
                 break;
 
             case 7:
-                day = 7;
+                numDay = 7;
                 nameDay = "Sunday";
                 break;
         }
@@ -102,9 +104,106 @@ public class MainActivity extends AppCompatActivity {
         return nameDay;
     }
 
-    class Timetable{
-        TextView lesson;
-        TextView homework;
+    public String dayInWeek(){
+        String nameDay = "";
+        switch (this.numDay){
+            case 1:
+                nameDay = "Monday";
+                break;
+
+            case 2:
+                nameDay = "Tuesday";
+                break;
+
+            case 3:
+                nameDay = "Wednesday";
+                break;
+
+            case 4:
+                nameDay = "Thursday";
+                break;
+
+            case 5:
+                nameDay = "Friday";
+                break;
+
+            case 6:
+                nameDay = "Saturday";
+                break;
+
+            case 7:
+                nameDay = "Sunday";
+                break;
+        }
+        return nameDay;
     }
 
+    public String dayInWeek(int numDay){
+        String nameDay = "";
+        switch (numDay){
+            case 1:
+                nameDay = "Monday";
+                break;
+
+            case 2:
+                nameDay = "Tuesday";
+                break;
+
+            case 3:
+                nameDay = "Wednesday";
+                break;
+
+            case 4:
+                nameDay = "Thursday";
+                break;
+
+            case 5:
+                nameDay = "Friday";
+                break;
+
+            case 6:
+                nameDay = "Saturday";
+                break;
+
+            case 7:
+                nameDay = "Sunday";
+                break;
+        }
+        return nameDay;
+    }
+
+
+    public int dayInWeek(String s){
+        int nameDay = 0;
+        switch (s){
+            case "Monday":
+                nameDay = 0;
+                break;
+
+            case "Tuesday":
+                nameDay = 1;
+                break;
+
+            case "Wednesday":
+                nameDay = 2;
+                break;
+
+            case "Thursday":
+                nameDay = 3;
+                break;
+
+            case "Friday":
+                nameDay = 4;
+                break;
+
+            case "Saturday":
+                nameDay = 5;
+                break;
+
+            case "Sunday":
+                nameDay = 6;
+                break;
+        }
+        return nameDay;
+    }
 }
